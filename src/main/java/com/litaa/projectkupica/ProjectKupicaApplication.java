87d8e4551c5df6370d6a14749b0ed3d065967117
Unagi_zoso <Unagi.zoso@gmail.com>
2022-09-29 16:22:11 +0900

    프로젝트 생성 . 초기 상태입니다.

---
+++ src/main/java/com/litaa/projectkupica/ProjectKupicaApplication.java
@@ -0,0 +1,13 @@
+package com.litaa.projectkupica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectKupicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectKupicaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/litaa/projectkupica/ProjectKupicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litaa.projectkupica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectKupicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
