b89e0fa91be9d08862a25c7f8a893d8cf69c014a
Unagi_zoso <Unagi.zoso@gmail.com>
2022-10-07 02:39:02 +0900

    Feat: LikeInfoRepository 추가

---
+++ src/main/java/com/litaa/projectkupica/domain/likeinfo/LikeInfoRepository.java
@@ -0,0 +1,10 @@
+package com.litaa.projectkupica.domain.likeinfo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author : Unagi_zoso
+ * @date : 2022-10-07
+ */
+public interface LikeInfoRepository extends JpaRepository<LikeInfo, Integer> {
+}
