1d1e33a435b6628697d4355c5a3e22f1a72c0f3d
Unagi_zoso <Unagi.zoso@gmail.com>
2022-10-06 23:58:49 +0900

    Test: PostRepository test코드 작성

---
+++ src/test/java/com/litaa/projectkupica/domain/post/PostRepositoryTest.java
@@ -0,0 +1,115 @@
+package com.litaa.projectkupica.domain.post;
+
+import com.litaa.projectkupica.domain.member.Member;
+import com.litaa.projectkupica.domain.member.MemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author : Unagi_zoso
+ * @date : 2022-10-06
+ */
+@DataJpaTest
+class PostRepositoryTest {
+
+    @Autowired
+    MemberRepository memRep;
+
+    @Autowired
+    PostRepository postRep;
+
+    @Autowired
+    EntityManager em;
+
+    Member member;
+
+    @BeforeEach
+    void beforeEach() {
+        member = Member.builder()
+            .memberNickname("개구리~")
+            .memberPassword("a1234")
+            .build();
+
+        memRep.save(member);
+    }
+
+    @AfterEach
+    void afterEach() {
+        Query q = em.createNativeQuery("ALTER TABLE post ALTER COLUMN post_id RESTART WITH 1");
+        q.executeUpdate();
+
+        q = em.createNativeQuery("ALTER TABLE member ALTER COLUMN member_id RESTART WITH 1");
+        q.executeUpdate();
+    }
+
+    @DisplayName("1. post 생성")
+    @Test
+    void test_1(){
+        Post newPost = Post.builder()
+                .postFkMember(member.getMemberId())
+                .build();
+
+        postRep.save(newPost);
+
+        assertEquals(postRep.findById(1).orElseThrow(RuntimeException::new).getPostFkMember(), 1);
+    }
+
+    @DisplayName("2. member의 post 목록 보기")
+    @Test
+    void test_2(){
+        Post newPost1 = Post.builder()
+                .postFkMember(member.getMemberId())
+                .build();
+
+        Post newPost2 = Post.builder()
+                .postFkMember(member.getMemberId())
+                .build();
+
+        Post newPost3 = Post.builder()
+                .postFkMember(member.getMemberId())
+                .build();
+
+        postRep.save(newPost1);
+        postRep.save(newPost2);
+        postRep.save(newPost3);
+
+        em.flush();
+
+        assertEquals(postRep.findAllByPostFkMember(member.getMemberId()).size(), 3);
+    }
+
+    @DisplayName("3. 전체 post 보기")
+    @Test
+    void test_3() {
+        Post newPost1 = Post.builder()
+                .postFkMember(1)
+                .build();
+
+        Post newPost2 = Post.builder()
+                .postFkMember(2)
+                .build();
+
+        Post newPost3 = Post.builder()
+                .postFkMember(3)
+                .build();
+
+        postRep.save(newPost1);
+        postRep.save(newPost2);
+        postRep.save(newPost3);
+
+        em.flush();
+
+        assertEquals(postRep.findAll().size(), 3);
+    }
+    // post 수정
+    // post 삭제
+}
