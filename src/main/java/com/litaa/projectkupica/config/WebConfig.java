1eb0bb3d25c66276af2ceefaf86745a6a4162ba0
Unagi_zoso <Unagi.zoso@gmail.com>
2022-12-31 23:46:12 +0900

    정적파일 탐색 위한 Web Config 설정

---
+++ src/main/java/com/litaa/projectkupica/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.litaa.projectkupica.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author : Unagi_zoso
+ * @date : 2022-12-30
+ */
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/templates/", "classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+    }
+}
