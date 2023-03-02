80499d40d89600935f259158db18f1e2b446d369
Unagi_zoso <Unagi.zoso@gmail.com>
2023-03-02 22:10:31 +0900

    Test
    Post attribute 수정으로 test 전체 수정

--- src/test/java/com/litaa/projectkupica/domain/post/PostRepositoryTest.java
+++ src/test/java/com/litaa/projectkupica/domain/post/PostRepositoryTest.java
@@ -1,13 +1,12 @@
 package com.litaa.projectkupica.domain.post;
 
-import com.litaa.projectkupica.domain.member.Member;
-import com.litaa.projectkupica.domain.member.MemberRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -19,11 +18,10 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
  * @author : Unagi_zoso
  * @date : 2022-10-06
  */
+
+@ActiveProfiles("local")
 @DataJpaTest
 class PostRepositoryTest {
-
-    @Autowired
-    MemberRepository memRep;
 
     @Autowired
     PostRepository postRep;
@@ -31,101 +29,60 @@ class PostRepositoryTest {
     @Autowired
     EntityManager em;
 
-    Member member;
+    Post post1, post2, post3;
 
     @BeforeEach
     void beforeEach() {
-        member = Member.builder()
-            .memberNickname("개구리~")
-            .memberPassword("a1234")
-            .build();
 
-        memRep.save(member);
+         post1 = Post.builder()
+                .source("/asdf1.jpg")
+                .caption("좀 많이 어렵네..")
+                .build();
+
+        post2 = Post.builder()
+                .source("/asdf2.jpg")
+                .caption("좀 많이 어렵네..")
+                .build();
+
+        post3 = Post.builder()
+                .source("/asdf3.jpg")
+                .caption("좀 많이 어렵네..")
+                .build();
+
+        postRep.save(post1);
+        postRep.save(post2);
+        postRep.save(post3);
     }
 
     @AfterEach
     void afterEach() {
         Query q = em.createNativeQuery("ALTER TABLE post ALTER COLUMN post_id RESTART WITH 1");
         q.executeUpdate();
-
-        q = em.createNativeQuery("ALTER TABLE member ALTER COLUMN member_id RESTART WITH 1");
-        q.executeUpdate();
     }
 
     @DisplayName("1. post 생성")
     @Test
     void test_1(){
-        Post newPost = Post.builder()
-                .caption("뇨로로롱")
-                .postFkMember(member)
-                .build();
 
-        postRep.save(newPost);
+        assertEquals(postRep.findById(1).orElseThrow(RuntimeException::new).getCaption(), "좀 많이 어렵네..");
+        assertEquals(postRep.findById(1).orElseThrow(RuntimeException::new).getPostId(), 1);
 
-        assertEquals(postRep.findById(1).orElseThrow(RuntimeException::new).getCaption(), "뇨로로롱");
-        assertEquals(postRep.findById(1).orElseThrow(RuntimeException::new).getPostFkMember(), 1);
+        em.flush();
     }
 
-    @DisplayName("2. member의 post 목록 보기")
+    @DisplayName("2. 전체 post 목록 가져오기")
     @Test
     void test_2(){
-        Post newPost1 = Post.builder()
-                .postFkMember(member)
-                .build();
-
-        Post newPost2 = Post.builder()
-                .postFkMember(member)
-                .build();
-
-        Post newPost3 = Post.builder()
-                .postFkMember(member)
-                .build();
-
-        postRep.save(newPost1);
-        postRep.save(newPost2);
-        postRep.save(newPost3);
-
-        em.flush();
-
-        assertEquals(postRep.findAllByPostFkMember(member.getMemberId()).size(), 3);
-    }
-
-    @DisplayName("3. 전체 post 보기")
-    @Test
-    void test_3() {
-        Post newPost1 = Post.builder()
-                .postFkMember(member)
-                .build();
-
-        Post newPost2 = Post.builder()
-                .postFkMember(member)
-                .build();
-
-        Post newPost3 = Post.builder()
-                .postFkMember(member)
-                .build();
-
-        postRep.save(newPost1);
-        postRep.save(newPost2);
-        postRep.save(newPost3);
-
-        em.flush();
 
         assertEquals(postRep.findAll().size(), 3);
+        em.flush();
     }
 
-    @DisplayName("4. post 삭제")
+    @DisplayName("3. post 삭제")
     @Test
     void test_4(){
-        Post newPost = Post.builder()
-                .postFkMember(member)
-                .build();
-
-        postRep.save(newPost);
 
         postRep.deleteById(1);
-
-        em.flush();
 
         assertFalse(postRep.findById(1).isPresent());
     }
