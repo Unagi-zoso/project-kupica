88a2581fb5c804def3ae30cbfa4ad8e9c52a3774
Unagi_zoso <Unagi.zoso@gmail.com>
2023-03-06 02:01:58 +0900

    Test: 포스트 페이지로 넘겨받기 test 코드 추가

--- src/test/java/com/litaa/projectkupica/web/PostControllerTest.java
+++ src/test/java/com/litaa/projectkupica/web/PostControllerTest.java
@@ -1,31 +1,40 @@
 package com.litaa.projectkupica.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.litaa.projectkupica.domain.post.Post;
 import com.litaa.projectkupica.service.PostService;
+import com.litaa.projectkupica.web.dto.PageDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.FileInputStream;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * @author : Unagi_zoso
  * @date : 2023-03-02
  */
 @ActiveProfiles("local")
+@WithMockUser(username = "테스트 관리자", roles = {"SUPER"})
 @WebMvcTest(PostController.class)
 class PostControllerTest {
-
     @Autowired
     ObjectMapper objectMapper;
 
@@ -37,7 +46,6 @@ class PostControllerTest {
 
     @DisplayName("포스트 업로드 하기")
     @Test
-    @WithMockUser(username = "테스트 관리자", roles = {"SUPER"})
     void uploadPostTest() throws Exception {
 
         final String fileName = "testimage1"; //파일명
@@ -57,7 +65,34 @@ class PostControllerTest {
                         .file(testImage1)
                         .param("password", "qwer1234")
                         .param("caption", "실로.. 좋은 사진이로다..")
-                        .with(csrf())
-        ).andExpect(status().isFound());
+                        .with(csrf()))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @DisplayName("포스트 페이지로 넘겨받기")
+    @Test
+    void findPostsByPageRequestTest() throws Exception {
+        List<Post> posts = Stream.of(
+                Post.builder().password("qwer1234").source("/asdf.jpg").caption("1이얏").build(),
+                Post.builder().password("qwer1234").source("/asdf.jpg").caption("2이얏").build(),
+                Post.builder().password("qwer1234").source("/asdf.jpg").caption("3이얏").build()).collect(Collectors.toList());
+
+        PageDto pageDto = PageDto.builder()
+                .lastPageId(0)
+                .defaultPageSize(3)
+                .build();
+
+        when(postService.findPostsByPageRequest(pageDto.getLastPageId(), pageDto.getDefaultPageSize())).thenReturn(posts);
+
+        String content = objectMapper.writeValueAsString(pageDto);
+        String ret = objectMapper.writeValueAsString(posts);
+
+        mockMvc.perform(post("/paging")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content)
+                        .with(csrf()))
+                .andDo(print())
+                .andExpect(content().json(ret));
     }
 }
---
+++ src/test/resources/schema-h2.sql
@@ -0,0 +1,9 @@
+CREATE TABLE post
+(
+    post_id    INT PRIMARY KEY AUTO_INCREMENT,
+    password   VARCHAR(14) NOT NULL,
+    source     VARCHAR(300) NOT NULL,
+    caption    TEXT     NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL
+);
