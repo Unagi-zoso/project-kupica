3f9a3e77218618b8ad9c4fd6ff6f55cc6e4d5b7d
Unagi_zoso <Unagi.zoso@gmail.com>
2023-03-12 23:25:42 +0900

    Feat: Post 테이블 속성 erase_flag 추가
    fdsa

--- src/test/resources/schema-h2.sql
+++ src/test/resources/schema-h2.sql
@@ -4,6 +4,7 @@ CREATE TABLE post
     password   VARCHAR(14) NOT NULL,
     source     VARCHAR(300) NOT NULL,
     caption    TEXT     NOT NULL,
+    erase_flag BOOL NOT NULL,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL
 );
