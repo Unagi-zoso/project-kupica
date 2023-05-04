de58ca2fd7b4939fd837c1b55526c2648f12cf36
Unagi_zoso <Unagi.zoso@gmail.com>
2023-05-05 01:57:57 +0900

    Feat: eraseFlag, downloadKey column 추가로 인한 수정

--- src/main/java/com/litaa/projectkupica/web/dto/PostDto.java
+++ src/main/java/com/litaa/projectkupica/web/dto/PostDto.java
@@ -1,28 +1,30 @@
 package com.litaa.projectkupica.web.dto;
-import com.litaa.projectkupica.domain.member.Member;
-import com.litaa.projectkupica.domain.photo.Photo;
+
 import com.litaa.projectkupica.domain.post.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.multipart.MultipartFile;
 
-/**
- * @author : Unagi_zoso
- * @date : 2022-12-31
- */
-
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class PostDto {
 
+    private String password;
     private MultipartFile file;
     private String caption;
+    private Integer eraseFlag;
 
-    public Post toEntity(String postPhotoUrl) {
+    public Post toEntity(String postPhotoUrl, String imageDownloadUrl) {
         return Post.builder()
+                .password(password)
                 .caption(caption)
                 .source(postPhotoUrl)
+                .downloadKey(imageDownloadUrl)
+                .eraseFlag(eraseFlag)
                 .build();
     }
 }
