c3b26a29cef2ed0302a4ae2ea0af46d923e3d9eb
Unagi_zoso <Unagi.zoso@gmail.com>
2023-05-05 02:02:59 +0900

    Feat: post 업로드를 위한 modal 관련 기능

--- src/main/resources/static/upload_modal.js
+++ src/main/resources/static/upload_modal.js
@@ -1,7 +1,14 @@
 document.getElementById("modal_open_btn").onclick = function() {
-    document.getElementById("modal").style.display="block";
+    document.getElementById("modal").style.display="flex"
 }
 
 document.getElementById("modal_close_btn").onclick = function() {
     document.getElementById("modal").style.display="none";
 }
+
+document.getElementById("modal").addEventListener("click", e => {
+    const evTarget = e.target
+    if(evTarget.classList.contains("modal-overlay")) {
+        document.getElementById("modal").style.display = "none"
+    }
+})
