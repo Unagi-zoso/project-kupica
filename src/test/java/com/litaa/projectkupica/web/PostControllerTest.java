018184a362e558cc43397a34786431cde3c9d047
Unagi_zoso <Unagi.zoso@gmail.com>
2023-05-05 02:10:40 +0900

    Test: PostController 함수명 변경, 새로운 함수 테스트코드 추가

--- src/test/java/com/litaa/projectkupica/web/PostControllerTest.java
+++ src/test/java/com/litaa/projectkupica/web/PostControllerTest.java
@@ -6,20 +6,28 @@ import com.litaa.projectkupica.service.PostService;
 import com.litaa.projectkupica.web.dto.PageDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
@@ -37,16 +45,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class PostControllerTest {
     @Autowired
     ObjectMapper objectMapper;
-
     @Autowired
     MockMvc mockMvc;
-
     @MockBean
     PostService postService;
 
-    @DisplayName("포스트 업로드 하기")
+    @DisplayName("post 업로드 하기")
     @Test
-    void uploadPostTest() throws Exception {
+    void testUploadPost() throws Exception {
 
         final String fileName = "testimage1"; //파일명
         final String contentType = "jpg"; //파일타입
@@ -70,14 +76,14 @@ class PostControllerTest {
                 .andExpect(redirectedUrl("/"));
     }
 
-    @DisplayName("포스트 페이지로 넘겨받기")
+    @DisplayName("post 페이지로 넘겨받기")
     @Test
-    void findPostsByPageRequestTest() throws Exception {
+    void testFindPostsByPageRequest() throws Exception {
 
         List<Post> posts = Stream.of(
-                Post.builder().password("qwer1234").source("/asdf.jpg").caption("1이얏").eraseFlag(false).build(),
-                Post.builder().password("qwer1234").source("/asdf.jpg").caption("2이얏").eraseFlag(false).build(),
-                Post.builder().password("qwer1234").source("/asdf.jpg").caption("3이얏").eraseFlag(false).build()).collect(Collectors.toList());
+                Post.builder().password("qwer1234").source("/asdf.jpg").caption("1이얏").downloadKey("s3://temp").eraseFlag(0).build(),
+                Post.builder().password("qwer1234").source("/asdf.jpg").caption("2이얏").downloadKey("s3://temp").eraseFlag(0).build(),
+                Post.builder().password("qwer1234").source("/asdf.jpg").caption("3이얏").downloadKey("s3://temp").eraseFlag(0).build()).collect(Collectors.toList());
 
         PageDto pageDto = PageDto.builder()
                 .lastPageId(0)
@@ -95,5 +101,37 @@ class PostControllerTest {
                         .with(csrf()))
                 .andDo(print())
                 .andExpect(content().json(ret));
+    }
+
+    @DisplayName("post 이미지 다운로드받기")
+    @Test
+    void testDownload() throws IOException {
+        PostService postService = Mockito.mock(PostService.class);
+        final String pathName = "src/test/resources/testimage/testimage1.jpg";
+        File file = new File(pathName);
+        byte[] mockImageData = Files.readAllBytes(file.toPath());
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.IMAGE_JPEG);
+        httpHeaders.setContentLength(mockImageData.length);
+        httpHeaders.setContentDispositionFormData("attachment", "testimage1");
+
+        when(postService.download(pathName)).thenReturn(new ResponseEntity<>(mockImageData, httpHeaders, HttpStatus.OK));
+
+        PostController postController = new PostController(postService);
+
+        // when
+        ResponseEntity<byte[]> response = postController.download(pathName);
+
+        // then
+        // response 상태 검증
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(MediaType.IMAGE_JPEG, response.getHeaders().getContentType());
+
+        // 이미지 데이터 검증
+        byte[] downloadedImageData = response.getBody();
+        assertNotNull(downloadedImageData);
+        assertEquals(mockImageData.length, downloadedImageData.length);
+        assertArrayEquals(mockImageData, downloadedImageData);
     }
 }
