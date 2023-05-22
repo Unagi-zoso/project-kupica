e219ca34c1501aebb10c100e394b33cc575c9189
Unagi_zoso <Unagi.zoso@gmail.com>
2023-05-22 16:12:42 +0900

    Feat: upload 시 이미지 미리보기 기능 구현

---
+++ src/main/resources/static/preview_image.js
@@ -0,0 +1,27 @@
+function previewImage(event) {
+  var input = event.target;
+  var preview = document.querySelector("#preview-image");
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      preview.innerHTML =
+        '<img src="' + e.target.result + '" class="img-fluid">';
+      var image = new Image();
+      image.src = e.target.result;
+      image.onload = function () {
+        var canvas = document.createElement("canvas");
+        var context = canvas.getContext("2d");
+        canvas.width = imgWidth = image.width;
+        canvas.height = imgHeight = image.height;
+        context.drawImage(image, 0, 0);
+        imageData = context.getImageData(0, 0, image.width, image.height).data;
+      };
+    };
+    reader.readAsDataURL(input.files[0]);
+  } else {
+    preview.innerHTML = "";
+    imageData = null;
+  }
+}
+
+document.querySelector("#image-chooser").addEventListener("change", previewImage);
