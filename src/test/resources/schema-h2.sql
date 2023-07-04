bedc193ace4d0446526ccf1a8d04d5fe458324aa
Unagi_zoso <Unagi.zoso@gmail.com>
2023-07-05 00:50:02 +0900

    Feat: cached_image_url 속성 추가

--- src/test/resources/schema-h2.sql
+++ src/test/resources/schema-h2.sql
@@ -1,9 +1,12 @@
 CREATE TABLE post
 (
-    post_id    INT PRIMARY KEY AUTO_INCREMENT,
-    password   VARCHAR(14) NOT NULL,
-    source     VARCHAR(300) NOT NULL,
-    caption    TEXT     NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL
+    post_id          INT PRIMARY KEY AUTO_INCREMENT,
+    password         VARCHAR(14) NOT NULL,
+    source           VARCHAR(300) NOT NULL,
+    cached_image_url VARCHAR(300) NOT NULL,
+    caption          TEXT     NOT NULL,
+    erase_flag       INT NOT NULL,
+    download_key     VARCHAR(300) NOT NULL,
+    created_at       DATETIME NOT NULL,
+    updated_at       DATETIME NOT NULL
 );
