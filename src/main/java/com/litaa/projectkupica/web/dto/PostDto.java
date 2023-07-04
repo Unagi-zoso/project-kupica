b76aa681aeeb8eab6698e53bfa1f8193c8ae3aa6
Unagi_zoso <Unagi.zoso@gmail.com>
2023-07-04 22:26:04 +0900

    Feat: 각 변수 유효성 검사 위한 제약조건 추가
    String password 제약조건 @NotBlank, @Pattern(4자리 이상 20자 이하
    공백제외 문자)
    Integer eraseFlag 제약조건 @Max(value = 1), @Min(value = 0)

--- src/main/java/com/litaa/projectkupica/web/dto/PostDto.java
+++ src/main/java/com/litaa/projectkupica/web/dto/PostDto.java
@@ -7,22 +7,31 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class PostDto {
 
+    @NotBlank
+    @Pattern(regexp = "^(?!\\s*$).{4,20}$", message = "비밀번호는 4자리 이상 20자 이하이어야 하며, 공백을 제외한 문자로 이루어져야 합니다.")
     private String password;
+
     private MultipartFile file;
     private String caption;
+
+    @Max(value = 1)
+    @Min(value = 0)
     private Integer eraseFlag;
 
-    public Post toEntity(String postPhotoUrl, String imageDownloadUrl) {
+    public Post toEntity(String postImageUrl, String cachedImageUrl, String imageDownloadUrl) {
         return Post.builder()
                 .password(password)
                 .caption(caption)
-                .source(postPhotoUrl)
+                .source(postImageUrl)
+                .cachedImageUrl(cachedImageUrl)
                 .downloadKey(imageDownloadUrl)
                 .eraseFlag(eraseFlag)
                 .build();
