846edaf27245ab9686205063654234cbc407fce3
Unagi_zoso <Unagi.zoso@gmail.com>
2023-07-11 17:23:54 +0900

    chore: password 데이터 타입 문자 최대 길이 160으로 수정

--- src/test/resources/schema-h2.sql
+++ src/test/resources/schema-h2.sql
@@ -1,7 +1,7 @@
 CREATE TABLE post
 (
     post_id          INT PRIMARY KEY AUTO_INCREMENT,
-    password         VARCHAR(14) NOT NULL,
+    password         VARCHAR(160) NOT NULL,
     source           VARCHAR(300) NOT NULL,
     cached_image_url VARCHAR(300) NOT NULL,
     caption          TEXT     NOT NULL,
