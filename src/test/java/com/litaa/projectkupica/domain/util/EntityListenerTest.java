94605ca52fc48c55e21806f2657bdd50f00bde8f
Unagi_zoso <Unagi.zoso@gmail.com>
2023-07-12 21:22:18 +0900

    Test: createdAt, updatedAt 이름 변경으로 인한 수정
    createdAt -> createdDateTime
    updatedAt -> updatedDateTime

--- src/test/java/com/litaa/projectkupica/domain/util/EntityListenerTest.java
+++ src/test/java/com/litaa/projectkupica/domain/util/EntityListenerTest.java
@@ -45,6 +45,7 @@ class EntityListenerTest {
                 .source("/asdf.jpg")
                 .cachedImageUrl("cf/asdf.jpg")
                 .caption("좋은 사진")
+                .erasedFlag(0)
                 .downloadKey("s3://temp")
                 .build();
 
@@ -61,14 +62,14 @@ class EntityListenerTest {
     @Test
     void When_CreatedAtIsNull_Then_False(){
 
-        assertNotNull(postRep.findById(1).orElseThrow(RuntimeException::new).getCreatedAt());
+        assertNotNull(postRep.findById(1).orElseThrow(RuntimeException::new).getCreatedDateTime());
     }
 
     @DisplayName("2. PreUpdate 데이터 입력 시 정상작동")
     @Test
     void When_UpdatedAtIsNull_Then_False(){
 
-        assertNotNull(postRep.findById(1).orElseThrow(RuntimeException::new).getUpdatedAt());
+        assertNotNull(postRep.findById(1).orElseThrow(RuntimeException::new).getUpdatedDateTime());
     }
 
     @DirtiesContext(methodMode = BEFORE_METHOD)
@@ -76,13 +77,13 @@ class EntityListenerTest {
     @Test
     void When_UpdateTimeWasNotUpdated_Then_False(){
 
-        LocalDateTime prevUpdate = postRep.findById(1).orElseThrow(RuntimeException::new).getUpdatedAt();
+        LocalDateTime prevUpdate = postRep.findById(1).orElseThrow(RuntimeException::new).getUpdatedDateTime();
 
         post1.setCaption("이 사진이 더 멋져!");
         postRep.save(post1);
         em.flush();
 
-        LocalDateTime curUpdate = postRep.findById(1).orElseThrow(RuntimeException::new).getUpdatedAt();
+        LocalDateTime curUpdate = postRep.findById(1).orElseThrow(RuntimeException::new).getUpdatedDateTime();
 
         assertNotEquals(prevUpdate, curUpdate);
 
