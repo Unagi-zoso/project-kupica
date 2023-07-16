bb83896754ecd6d5b1306ddb55a3c1603ab3a90f
Unagi_zoso <Unagi.zoso@gmail.com>
2023-07-17 00:22:04 +0900

    Test: Image 테이블 분리로 인한 테스트 코드 수정

--- src/test/java/com/litaa/projectkupica/web/PostControllerTest.java
+++ src/test/java/com/litaa/projectkupica/web/PostControllerTest.java
@@ -1,20 +1,17 @@
 package com.litaa.projectkupica.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.litaa.projectkupica.domain.post.Post;
+import com.litaa.projectkupica.domain.post.Post.PostResponse;
 import com.litaa.projectkupica.service.PostService;
 import com.litaa.projectkupica.web.dto.DeletePostFormDto;
-import com.litaa.projectkupica.web.dto.PageDto;
-import com.litaa.projectkupica.web.dto.PostDto;
 import com.litaa.projectkupica.web.dto.UpdatePostFormDto;
+import com.litaa.projectkupica.web.dto.UploadPostFormDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -22,20 +19,17 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
@@ -52,14 +46,13 @@ class PostControllerTest {
     @Autowired
     MockMvc mockMvc;
     @MockBean
-    PostService postService;
-
+    PostService postServiceMock;
     @Autowired
     PostController postController;
 
     /**
      * @throws Exception
-     * @see com.litaa.projectkupica.service.PostService#uploadPost(PostDto)
+     * @see com.litaa.projectkupica.service.PostService#uploadPost(UploadPostFormDto)
      */
     @DisplayName("post 업로드 하기")
     @Test
@@ -77,17 +70,17 @@ class PostControllerTest {
                 fileInputStream
         );
 
-        PostDto postDto = PostDto.builder()
+        UploadPostFormDto postDto = UploadPostFormDto.builder()
                 .file(file)
                 .password("qwer1234")
                 .caption("실로.. 좋은 사진이로다..")
                 .erasedFlag(0)
                 .build();
 
-        when(postService.uploadPost(any(PostDto.class))).thenReturn(new ResponseEntity<>(HttpStatus.OK));
+        when(postServiceMock.uploadPost(any(UploadPostFormDto.class))).thenReturn(new ResponseEntity<>(HttpStatus.OK));
 
         mockMvc.perform(
-                        multipart("/post/upload")
+                        multipart("/posts")
                                 .file((MockMultipartFile) postDto.getFile())
                                 .param("password", postDto.getPassword())
                                 .param("caption", postDto.getCaption())
@@ -95,126 +88,94 @@ class PostControllerTest {
                                 .with(csrf()))
                 .andExpect(MockMvcResultMatchers.status().isOk());
 
-        verify(postService, times(1)).uploadPost(any(PostDto.class));
+        verify(postServiceMock, times(1)).uploadPost(any(UploadPostFormDto.class));
     }
 
     @DisplayName("post 페이지로 넘겨받기")
     @Test
     void testFindPostsByPageRequest() throws Exception {
 
-        List<Post> posts = Stream.of(
-                Post.builder().password("qwer1234").source("/asdf.jpg").caption("1이얏").downloadKey("s3://temp").erasedFlag(0).build(),
-                Post.builder().password("qwer1234").source("/asdf.jpg").caption("2이얏").downloadKey("s3://temp").erasedFlag(0).build(),
-                Post.builder().password("qwer1234").source("/asdf.jpg").caption("3이얏").downloadKey("s3://temp").erasedFlag(0).build()).collect(Collectors.toList());
+        List<PostResponse> posts = Stream.of(
+                PostResponse.builder().postId(1).caption("test caption").source("s3://source").cachedImageUrl("cf://source").downloadKey("down://source").build(),
+                PostResponse.builder().postId(2).caption("test caption").source("s3://source").cachedImageUrl("cf://source").downloadKey("down://source").build(),
+                PostResponse.builder().postId(3).caption("test caption").source("s3://source").cachedImageUrl("cf://source").downloadKey("down://source").build()).collect(Collectors.toList());
 
-        PageDto pageDto = PageDto.builder()
-                .lastPageId(0)
-                .defaultPageSize(3)
-                .build();
+        int lastPageId = 0;
+        int defaultPageSize = 3;
 
-        when(postService.findPostsByPageRequest(pageDto.getLastPageId(), pageDto.getDefaultPageSize())).thenReturn(posts);
+        when(postServiceMock.findPostsByPageRequest(lastPageId, defaultPageSize)).thenReturn(posts);
 
-        String content = objectMapper.writeValueAsString(pageDto);
         String ret = objectMapper.writeValueAsString(posts);
 
-        mockMvc.perform(post("/paging")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(content)
+        mockMvc.perform(get("/posts/page")
+                        .param("lastPageId", Integer.toString(lastPageId))
+                        .param("pageSize", Integer.toString(defaultPageSize))
                         .with(csrf()))
                 .andDo(print())
                 .andExpect(content().json(ret));
-    }
-
-    @DisplayName("post 이미지 다운로드받기")
-    @Test
-    void testDownload() throws IOException {
-
-        final String pathName = "src/test/resources/testimage/testimage1.jpg";
-        File file = new File(pathName);
-        byte[] mockImageData = Files.readAllBytes(file.toPath());
-
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.IMAGE_JPEG);
-        httpHeaders.setContentLength(mockImageData.length);
-        httpHeaders.setContentDispositionFormData("attachment", "testimage1");
-
-
-        when(postService.download(pathName)).thenReturn(new ResponseEntity<>(mockImageData, httpHeaders, HttpStatus.OK));
-
-        // when
-        ResponseEntity<byte[]> response = postController.download(pathName);
-
-        // then
-        // response 상태 검증
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(MediaType.IMAGE_JPEG, response.getHeaders().getContentType());
-
-        // 이미지 데이터 검증
-        byte[] downloadedImageData = response.getBody();
-        assertNotNull(downloadedImageData);
-        assertEquals(mockImageData.length, downloadedImageData.length);
-        assertArrayEquals(mockImageData, downloadedImageData);
     }
 
     @DisplayName("post 삭제하기 ")
     @Test
     void testDeletePost() {
 
+        int postId = 1;
         DeletePostFormDto deletePostFormDto = DeletePostFormDto.builder()
-                .id(1)
                 .password("qwer1234")
                 .build();
 
-        when(postService.updatePostErasedTrue(deletePostFormDto.getId(), deletePostFormDto.getPassword()))
-                .thenReturn(new ResponseEntity<>(HttpStatus.OK));
+        when(postServiceMock.updatePostErasedTrue(postId, deletePostFormDto.getPassword())).thenReturn(new ResponseEntity<>(HttpStatus.OK));
 
         // when
-        ResponseEntity<?> response = postController.deletePost(deletePostFormDto);
-
-        // then
-        assertEquals(HttpStatus.OK, response.getStatusCode());
+        try {
+            mockMvc.perform(delete("/posts/{postId}/delete", postId)
+                            .param("password", deletePostFormDto.getPassword())
+                            .with(csrf())
+                    )
+                    .andExpect(MockMvcResultMatchers.status().isOk());
+        } catch (Exception ignored) {
+        }
     }
 
     @DisplayName("post 수정하기 ")
     @Test
     void testUpdatePost() throws IOException {
 
-        final String fileName = "testimage1"; //파일명
-        final String contentType = "jpg"; //파일타입
-        final String filePath = "src/test/resources/testimage/" + fileName + "." + contentType; //파일경로
+        final String fileName = "testimage1";
+        final String contentType = "jpg";
+        final String filePath = "src/test/resources/testimage/" + fileName + "." + contentType;
         FileInputStream fileInputStream = new FileInputStream(filePath);
 
         MockMultipartFile testImage1 = new MockMultipartFile(
-                "testImage1", //name
-                fileName + "." + contentType, //originalFilename
+                "testImage1",
+                fileName + "." + contentType,
                 contentType,
                 fileInputStream
         );
 
         UpdatePostFormDto updatePostFormDto = UpdatePostFormDto.builder()
-                .id(1)
                 .caption("해는 뜬다.")
                 .file(testImage1)
                 .password("qwer1234")
                 .build();
 
-        when(postService.updatePost(updatePostFormDto))
+        when(postServiceMock.updatePost(1, updatePostFormDto))
                 .thenReturn(new ResponseEntity<>(HttpStatus.OK));
 
         // when
-        ResponseEntity<?> response = postController.updatePost(updatePostFormDto);
+        ResponseEntity<?> response = postController.updatePost(1, updatePostFormDto);
 
         // then
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
-    @DisplayName("PostDto 유효성 검사")
+    @DisplayName("UploadPostFormDto 유효성 검사")
     @Test
-    void testPostDtoValidation_Password() throws IOException {
+    void testUploadPostFormDtoValidation_Password() throws IOException {
 
-        final String fileName = "testimage1"; //파일명
-        final String contentType = "jpg"; //파일타입
-        final String filePath = "src/test/resources/testimage/" + fileName + "." + contentType; //파일경로
+        final String fileName = "testimage1";
+        final String contentType = "jpg";
+        final String filePath = "src/test/resources/testimage/" + fileName + "." + contentType;
         FileInputStream fileInputStream = new FileInputStream(filePath);
 
         MockMultipartFile file = new MockMultipartFile(
@@ -224,18 +185,18 @@ class PostControllerTest {
                 fileInputStream
         );
 
-        PostDto postDto = PostDto.builder()
+        UploadPostFormDto postDto = UploadPostFormDto.builder()
                 .file(file)
                 .password("qwe")
                 .caption("안녕하세요?")
                 .erasedFlag(0)
                 .build();
 
-        when(postService.uploadPost(any(PostDto.class))).thenReturn(new ResponseEntity<>(HttpStatus.OK));
+        when(postServiceMock.uploadPost(any(UploadPostFormDto.class))).thenReturn(new ResponseEntity<>(HttpStatus.OK));
 
         try {
             mockMvc.perform(
-                            multipart("/post/upload")
+                            multipart("/posts")
                                     .file((MockMultipartFile) postDto.getFile())
                                     .param("password", postDto.getPassword())
                                     .param("caption", postDto.getCaption())
@@ -246,12 +207,12 @@ class PostControllerTest {
         }
     }
 
-    @DisplayName("PostDto 유효성 검사")
+    @DisplayName("UploadPostFormDto 유효성 검사")
     @Test
-    void testPostDtoValidation_erasedFlag() throws IOException {
+    void testUploadPostFormDtoValidation_erasedFlag() throws IOException {
 
-        final String fileName = "testimage1"; //파일명
-        final String contentType = "jpg"; //파일타입
+        final String fileName = "testimage1";
+        final String contentType = "jpg";
         final String filePath = "src/test/resources/testimage/" + fileName + "." + contentType; //파일경로
         FileInputStream fileInputStream = new FileInputStream(filePath);
 
@@ -262,18 +223,18 @@ class PostControllerTest {
                 fileInputStream
         );
 
-        PostDto postDto = PostDto.builder()
+        UploadPostFormDto postDto = UploadPostFormDto.builder()
                 .file(file)
                 .password("qwe1234")
                 .caption("안녕하세요?")
                 .erasedFlag(5)
                 .build();
 
-        when(postService.uploadPost(any(PostDto.class))).thenReturn(new ResponseEntity<>(HttpStatus.OK));
+        when(postServiceMock.uploadPost(any(UploadPostFormDto.class))).thenReturn(new ResponseEntity<>(HttpStatus.OK));
 
         try {
             mockMvc.perform(
-                            multipart("/post/upload")
+                            multipart("/posts")
                                     .file((MockMultipartFile) postDto.getFile())
                                     .param("password", postDto.getPassword())
                                     .param("caption", postDto.getCaption())
