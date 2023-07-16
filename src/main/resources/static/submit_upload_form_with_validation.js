73690b31336b3143882dd02cad77e6f46d1682b3
Unagi_zoso <Unagi.zoso@gmail.com>
2023-07-17 00:14:28 +0900

    Feat: upload URL 변경, 파일명 submit_upload_form_with_validation으로 변경

---
+++ src/main/resources/static/submit_upload_form_with_validation.js
@@ -0,0 +1,49 @@
+function submitUploadForm(event) {
+    event.preventDefault(); // 폼 제출 기본 동작 막기
+
+    var fileInput = document.getElementById('image-chooser');
+    var selectedFile = fileInput.files[0];
+    var allowedExtensions = ['.jpg', '.png', '.gif', '.jpeg'];
+
+    if (selectedFile) {
+        var fileExtension = selectedFile.name.split('.').pop().toLowerCase();
+
+        if (!allowedExtensions.includes('.' + fileExtension)) {
+            alert('선택한 파일의 확장자가 허용되지 않습니다.');
+            return;
+        }
+
+        var formData = new FormData();
+
+        var captionInput = document.getElementById("caption-input");
+        var passwordInput = document.getElementById("password-input");
+
+        formData.append('file', selectedFile);
+        formData.append('caption', captionInput.value);
+        formData.append('password', passwordInput.value);
+
+        // Fetch 요청 보내기
+        fetch('posts', {
+            method: 'POST',
+            body: formData
+        })
+            .then(function(response) {
+
+                if (response.ok) {
+                    resetModalUploadForm();
+                } else {
+                    alert('파일 업로드 실패!');
+                }
+            }) .catch(reason => {
+                console.error(reason);
+        })
+
+    }
+}
+
+function resetModalUploadForm() {
+    document.getElementById('modal-upload').style.display = "none";
+    document.getElementById('preview-image').innerHTML = "";
+    document.getElementById('file-info').innerHTML = "";
+    document.getElementById('upload-form').reset();
+}
