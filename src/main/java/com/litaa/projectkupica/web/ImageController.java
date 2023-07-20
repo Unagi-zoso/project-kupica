637505db1c40eae70c4ef809593e75d3c693327a
Unagi_zoso <Unagi.zoso@gmail.com>
2023-07-20 23:53:04 +0900

    Refactor: download 메서드 매핑 URI 변경, httpheader 제작
    기존 download 메서드 URI
    /images/{downloadKey}/download -> /images/{imageId}/download
    서비스레이어에서 만들어서 반환한 httpheader를 컨트롤러레이어에서 만든다.

--- src/main/java/com/litaa/projectkupica/web/ImageController.java
+++ src/main/java/com/litaa/projectkupica/web/ImageController.java
@@ -2,9 +2,15 @@ package com.litaa.projectkupica.web;
 
 import com.litaa.projectkupica.domain.image.Image.ImageResponse;
 import com.litaa.projectkupica.service.ImageService;
+import com.litaa.projectkupica.web.dto.ImageFile;
+import com.litaa.projectkupica.web.model.ImageListModel;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,28 +31,38 @@ public class ImageController {
     private final ImageService imageService;
     private final Logger LOGGER = LoggerFactory.getLogger(PostController.class);
 
-    @GetMapping("/images/{downloadKey}/download")
-    public ResponseEntity<byte[]> download(@PathVariable String downloadKey) throws IOException {
+    @GetMapping("/images/{imageId}/download")
+    public ResponseEntity<byte[]> download(@PathVariable int imageId) throws IOException {
 
         long startTime = System.currentTimeMillis();
-        LOGGER.info("[ImageController] download post. downloadImageUrl : {}", downloadKey);
-        ResponseEntity<byte[]> response = imageService.download(downloadKey);
+        LOGGER.info("[ImageController] download post. downloadImageUrl : {}", imageId);
+
+        ImageFile imageFile = imageService.download(imageId);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(imageFile.getMediaType());
+        httpHeaders.setContentLength(imageFile.getSize());
+        httpHeaders.setContentDisposition(ContentDisposition.attachment().filename(imageFile.getFileName()).build());
 
         LOGGER.info("[ImageController] download post processing time : {}", (System.currentTimeMillis() - startTime));
-        return response;
+
+        return new ResponseEntity<>(imageFile.getBytes(), httpHeaders, HttpStatus.OK);
     }
 
     @GetMapping("/images/latest/5")
-    public List<ImageResponse> findLatestImages5() {
+    public ResponseEntity<ImageListModel> findLatestImages5() {
 
         long startTime = System.currentTimeMillis();
         LOGGER.info("[ImageController] find latest 5 images .");
 
         List<ImageResponse> imageResponses = imageService.findLatestImages5();
 
+        ImageListModel imageListModel = new ImageListModel(imageResponses);
+        imageListModel.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(ImageController.class).findLatestImages5()).withSelfRel());
+
         LOGGER.info("[ImageController] find latest 5 images. post size : {}", imageResponses.size());
         LOGGER.info("[ImageController] find latest 5 images processing time : {}", (System.currentTimeMillis() - startTime));
 
-        return imageResponses;
+        return new ResponseEntity<>(imageListModel, HttpStatus.OK);
     }
 }
