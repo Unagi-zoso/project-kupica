582e1b9a2a77649a40d8c3cb2775bec0047d1fd6
Unagi_zoso <Unagi.zoso@gmail.com>
2023-08-11 01:56:04 +0900

    Test: @ExtendWith(RestDocumentationExtension.class) 제거 의존성 충돌 의심

--- src/test/java/com/litaa/projectkupica/web/PostControllerTest.java
+++ src/test/java/com/litaa/projectkupica/web/PostControllerTest.java
@@ -43,7 +43,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @ActiveProfiles("local")
 @WithMockUser(username = "테스트 관리자", roles = {"SUPER"})
-@ExtendWith(RestDocumentationExtension.class)
 @AutoConfigureRestDocs
 @WebMvcTest(PostController.class)
 class PostControllerTest {
