541509b34090bb6343788587a43bb80035c655b4
Unagi_zoso <Unagi.zoso@gmail.com>
2023-08-11 02:58:59 +0900

    Test: @ExtendWith(RestDocumentationExtension.class) 추가

--- src/test/java/com/litaa/projectkupica/web/ImageControllerTest.java
+++ src/test/java/com/litaa/projectkupica/web/ImageControllerTest.java
@@ -37,6 +37,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ActiveProfiles("local")
 @WithMockUser(username = "테스트 관리자", roles = {"SUPER"})
+@ExtendWith(RestDocumentationExtension.class)
 @AutoConfigureRestDocs
 @WebMvcTest(ImageController.class)
 class ImageControllerTest {
